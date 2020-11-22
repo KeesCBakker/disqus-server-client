713fc0f1ff3b9e34d52ffb21fdde6a09efb74826
Kees C. Bakker <k.bakker@hostedsolutions.nu>
2020-11-22 22:16:29 +0100

    Licenses

--- packages/disqus-server-client-core/src/index.ts
+++ packages/disqus-server-client-core/src/index.ts
@@ -1,3 +1,12 @@
+/*!
+ * Disqus Server Client - Core
+ * https://github.com/KeesCBakker/disqus-server-client/
+ * http://keestalkstech.com
+ *
+ * Copyright Kees C. Bakker / KeesTalksTech
+ * Released under the MIT license
+ */
+
 import { IGenericDisqusResponse } from "./IGenericDisqusResponse"
 import { actionRequiringPost } from "./data/posts"
 import { cachedActions } from "./data/cached"
--- packages/disqus-server-client/src/index.ts
+++ packages/disqus-server-client/src/index.ts
@@ -1,3 +1,12 @@
+/*!
+ * Disqus Server Client
+ * https://github.com/KeesCBakker/disqus-server-client/
+ * http://keestalkstech.com
+ *
+ * Copyright Kees C. Bakker / KeesTalksTech
+ * Released under the MIT license
+ */
+
 import { Disqus } from "./disqus"
 
 export { Disqus }
